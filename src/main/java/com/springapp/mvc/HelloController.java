ab25535155923e789eb092d584b985ac5d61e5ce
dungGrooovy <mayonesama12@gmail.com>
2016-06-15 16:26:59 +0700

    first

---
+++ src/main/java/com/springapp/mvc/HelloController.java
@@ -0,0 +1,18 @@
+package com.springapp.mvc;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+@RequestMapping("/")
+public class HelloController {
+	@RequestMapping(method = RequestMethod.GET)
+	public ModelAndView homePage() {
+		ModelAndView model = new ModelAndView();
+		model.addObject("message","Yeah you get it!");
+		model.setViewName("hello");
+		return model;
+	}
+}
---
+++ src/main/java/com/springapp/mvc/model/User.java
@@ -0,0 +1,61 @@
+package com.springapp.mvc.model;
+
+import javax.management.relation.Role;
+import javax.persistence.*;
+import java.util.Set;
+
+/**
+ * Created by quangdung on 15/06/2016.
+ */
+@Entity
+@Table(name = "user")
+public class User {
+    private Long id;
+    private String username;
+    private String password;
+    private String passwordConfirm;
+    private Set<Role> roles;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getPasswordConfirm() {
+        return passwordConfirm;
+    }
+
+    public void setPasswordConfirm(String passwordConfirm) {
+        this.passwordConfirm = passwordConfirm;
+    }
+
+
+    public Set<Role> getRoles() {
+        return roles;
+    }
+
+    public void setRoles(Set<Role> roles) {
+        this.roles = roles;
+    }
+
+
+}
